8ab9b21775c06ca24780d5742821f7388881e713
hickey <596310898@163.com>
2024-03-25 12:00:25 +0800

    dev: interactive the wais api

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
